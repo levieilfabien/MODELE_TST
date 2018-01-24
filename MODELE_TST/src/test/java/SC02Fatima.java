d1d1a8e611364aa0cbf4826f15726cb3f00de600
ichiroufa <ichiroufa@DK285634.cib.net>
2018-01-24 16:34:51 +0100

    Commit Fatima

--- MODELE_TST/src/test/java/SC02Fatima.java
+++ MODELE_TST/src/test/java/SC02Fatima.java
@@ -1,18 +1,12 @@
 package test.java;
 
-import java.io.File;
-
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
 import constantes.Actions;
 import exceptions.SeleniumException;
 import main.bean.CasEssaiModeleBean;
 import main.constantes.Cibles;
 import main.constantes.Constantes;
-import moteurs.FirefoxImpl;
-import moteurs.GenericDriver;
 import outils.SeleniumOutils;
 
 public class SC02Fatima extends SC00Modele {
@@ -45,33 +39,10 @@ public class SC02Fatima extends SC00Modele {
 		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_email_SAMY, "ichiroubh@gmail.com"); 
 		//Saisir le numéro de portable 
 		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_telPortable_SAMY, "0610109436"); 
-	}
-	
-	/**
-	 * Permet d'obtenir la boite a outil Selenium associe a un driver pour le scenario donne.
-	 * @param scenario0 le scenario concerne.
-	 * @return la boite a outil Selenium associee au scenario.
-	 */
-	public SeleniumOutils obtenirDriver(CasEssaiModeleBean scenario0) {
-		//Configuration du driver
-		FirefoxBinary ffBinary = new FirefoxBinary(new File(Constantes.EMPLACEMENT_FIREFOX));
-		FirefoxProfile profile = configurerProfilNatixis();
-
-		if (scenario0.getRepertoireTelechargement() == null) { 
-			String repertoire = creerRepertoireTelechargement(scenario0, profile);
-			scenario0.setRepertoireTelechargement(repertoire);
-			this.setRepertoireTelechargement(repertoire);
-		}
-		// Initialisation du driver
-		//FirefoxImpl driver = new FirefoxImpl(ffBinary, profile);
-		FirefoxImpl driver = new FirefoxImpl(profile);
-		
-		driver.get(Constantes.URL_SAMY_FATIMA);
-
-	    SeleniumOutils outil = new SeleniumOutils(driver, GenericDriver.FIREFOX_IMPL);
-	    outil.setRepertoireRacine(scenario0.getRepertoireTelechargement());
-		
-		return outil;
+        // Remplir le formulaire et le valider
+        outil.action(Actions.VIDER_ET_SAISIR, Cibles.SAISIE_MONTANT_EPARGNE, "5000");
+        outil.action(Actions.VIDER_ET_SAISIR, Cibles.SAISIE_DATE_DISPO, "15/10/2018");
+        outil.action(Actions.CLIQUER, Cibles.VALIDER_FORMULAIRE_DEMO);
 	}
 }
 
