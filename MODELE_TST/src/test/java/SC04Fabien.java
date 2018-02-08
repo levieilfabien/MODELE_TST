e99043aaa5008ffbd872096dd0346348f16a2edd
levieilfa <levieilfa@DK277176.cib.net>
2018-02-08 16:22:43 +0100

    Ajout de l'export template

--- MODELE_TST/src/test/java/SC04Fabien.java
+++ MODELE_TST/src/test/java/SC04Fabien.java
@@ -34,5 +34,11 @@ public class SC04Fabien extends SC00Modele {
 		outil.action(Actions.SELECTIONNER, Cibles.SELECTION_CIVILITE_SAMY, "HOMME"); 
 		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_NOM_SAMY, "LEMONSIEUR"); 
 		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_PRENOM_SAMY, "KEVIN"); 
+		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_dateNaissance_SAMY, "12/12/1970"); 
+		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_email_SAMY, "ichiroubh@gmail.com"); 
+		outil.action(Actions.VIDER_ET_SAISIR, Cibles.SELECTION_telPortable_SAMY, "0610109436"); 
+        outil.action(Actions.VIDER_ET_SAISIR, Cibles.SAISIE_MONTANT_EPARGNE, "5000");
+        outil.action(Actions.VIDER_ET_SAISIR, Cibles.SAISIE_DATE_DISPO, "15/10/2018");
+        outil.action(Actions.CLIQUER, Cibles.VALIDER_FORMULAIRE_DEMO);
 	}
 }
